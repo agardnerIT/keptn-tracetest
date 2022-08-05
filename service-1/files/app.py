03160fdcd87910c190e69d082808c9e0f61c6a30
Adam Gardner <adam@agardner.net>
2022-08-05 12:30:30 +1000

    Update app.py

--- service-1/files/app.py
+++ service-1/files/app.py
@@ -14,47 +14,55 @@ test_result = subprocess.run([
 
 test_result_json = json.loads(test_result.stdout)
 
-check_results = []
+#####################
+# v1 Logic: Parse the results and fail the Keptn task if ALL tracetest checks FAIL
+# Cause the task to fail simply by outputting a non-zero exit code
+# According to a discussion on discord with schoren, apparently this is even easier:
+# if .testRun.result.allPassed is true then all checks have passed
+# if .testRun.result.allPassed is missing or false, we can assume some checks have failed
+#####################
 
-for selector_result in test_result_json['testRun']['result']['results']:
+#check_results = []
 
-    selector_query = selector_result['selector']['query']
+#for selector_result in test_result_json['testRun']['result']['results']:
+
+#    selector_query = selector_result['selector']['query']
     #print(f"Got selector_query: {selector_query}")
 
-    for assertion_result in selector_result['results']:
+#    for assertion_result in selector_result['results']:
 
-        assertion_name = assertion_result['assertion']['attribute']
-        if "allPassed" in assertion_result:
-            #print(f"Assertion: {assertion_name} passed for selector: {selector_query}")
-            check_results.append({
-                "name": assertion_name,
-                "selector": selector_query,
-                "status": "Pass"
-            })
-        else:
-            #print(f"Assertion: {assertion_name} failed for selector: {selector_query}")
-            check_results.append({
-                "name": assertion_name,
-                "selector": selector_query,
-                "status": "Fail"
-            })
+#        assertion_name = assertion_result['assertion']['attribute']
+#        if "allPassed" in assertion_result:
+#            #print(f"Assertion: {assertion_name} passed for selector: {selector_query}")
+#            check_results.append({
+#                "name": assertion_name,
+#                "selector": selector_query,
+#                "status": "Pass"
+#            })
+#        else:
+#            #print(f"Assertion: {assertion_name} failed for selector: {selector_query}")
+#            check_results.append({
+#                "name": assertion_name,
+#                "selector": selector_query,
+#                "status": "Fail"
+#            })
 
 # Output Results for Checks.
-passed_checks = sum(1 for check in check_results if check['status'] == "Pass")
-failed_checks = sum(1 for check in check_results if check['status'] == "Fail")
-pass_percentage = round(passed_checks / len(check_results) * 100)
+#passed_checks = sum(1 for check in check_results if check['status'] == "Pass")
+#failed_checks = sum(1 for check in check_results if check['status'] == "Fail")
+#pass_percentage = round(passed_checks / len(check_results) * 100)
 
-print(f"{passed_checks} checks passed")
-print(f"{failed_checks} checks failed")
-print(f"Pass percentage: {pass_percentage}%")
+#print(f"{passed_checks} checks passed")
+#print(f"{failed_checks} checks failed")
+#print(f"Pass percentage: {pass_percentage}%")
 
-for check in check_results:
-    print(f"Assertion: {check['name']} for selector: {check['selector']} status: {check['status']}")
+#for check in check_results:
+#    print(f"Assertion: {check['name']} for selector: {check['selector']} status: {check['status']}")
 
-if pass_percentage == 0:
-    print("-------------")
-    print("No tests passed, failing the task...")
+#if pass_percentage == 0:
+#    print("-------------")
+#    print("No tests passed, failing the task...")
     # If this script exits with a non-zero exit code, the task will fail as desired
-    exit(1)
+#    exit(1)
 
 #print(check_results)
